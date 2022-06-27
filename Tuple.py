b1900fd3142d7dc2b9e5e5fcdf327bd571c4685b
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-27 12:41:02 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,25 @@
+# n=(5)
+# print(n,type(n)) #5 <class 'int'>
+
+# x=(2,)
+# print(x,type(x))  #(2,) <class 'tuple'>
+
+# a=('hyd','nlg','srpt','mrlg')
+# a[1]='nzb'  #TypeError: 'tuple' object does not support item assignment
+
+# m="hai","hey"
+# print(m,type(m)) #('hai', 'hey') <class 'tuple'>
+# print(m[1])  #hey
+# print(m[2])  #IndexError: tuple index out of range
+
+# a=(1,3.4,"hai",None,True,[4,5],('y','t'))
+# print(a,type(a))  #(1, 3.4, 'hai', None, True, [4, 5], ('y', 't')) <class 'tuple'>
+
+# a=(1,3.4,"hai",None,True,[4,5],('y','t'))
+# # print(a.count(3.4)) #1
+# print(a.index([4,5])) #5
+
+
+
+
+
